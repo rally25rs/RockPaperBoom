9c52dfead7e5bb988f6322e16deee160db9c73a2
Rich Hildebrand <Rich@Clear-Measure.com>
2014-09-23 21:23:19 -0400

    mocha will now look into recursive folders

--- test/logic/gameScorer/didPlayerOneWin.js
+++ test/logic/gameScorer/didPlayerOneWin.js
@@ -1,7 +1,7 @@
 var expect = require("chai").expect;
 
 describe('didPlayerOneWin', function() {
-	var gameScorer = require('../logic/gameScorer');
+	var gameScorer = require('../../../logic/gameScorer');
 
   	it('should return true when rock plays scissors.',function() {
     	var playerOneWins = gameScorer.didPlayerOneWin('R', 'S');
